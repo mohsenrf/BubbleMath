5b5dae4976131dbd87fa20c12039ea978a6981ea
mohsenrf <mohsenrf@gmail.com>
2013-05-12 01:32:27 +1000

    Initial Commit

---
+++ BubbleMath/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BubbleMath
+//
+//  Created by Mohsen Rahimifar on 12/05/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}

5466a641d345376564a3136844d7051cf0bcfdc4
Mohsen Rahimifar <mohsenrf@gmail.com>
2013-05-14 03:01:25 +1000

    added particle generator class.

---
+++ BubbleMath/ParticleGenerator.h
@@ -0,0 +1,13 @@
+//
+//  ParticleGenerator.h
+//  BubbleMath
+//
+//  Created by Mohsen Rahimifar on 14/05/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParticleGenerator : NSObject
+
+@end
---
+++ BubbleMath/ParticleGenerator.m
@@ -0,0 +1,13 @@
+//
+//  ParticleGenerator.m
+//  BubbleMath
+//
+//  Created by Mohsen Rahimifar on 14/05/13.
+//
+//
+
+#import "ParticleGenerator.h"
+
+@implementation ParticleGenerator
+
+@end
